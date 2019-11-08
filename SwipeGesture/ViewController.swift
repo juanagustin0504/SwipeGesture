bc7c6f2dfa0c614fcd97dbd7cf0812c36627f9bb
juanagustin0504 <juanagustin@naver.com>
2019-11-08 15:37:35 +0900

    add : multi swipe

--- SwipeGesture/ViewController.swift
+++ SwipeGesture/ViewController.swift
@@ -27,12 +27,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading th e view.
         imgUp.append(UIImage(named: "short-arrow-up.png")!)
         imgUp.append(UIImage(named: "long-arrow-up.png")!)
+        imgUp.append(UIImage(named: "chevron-arrow-up.png")!)
         imgDown.append(UIImage(named: "short-arrow-down.png")!)
         imgDown.append(UIImage(named: "long-arrow-down.png")!)
+        imgDown.append(UIImage(named: "chevron-arrow-down.png")!)
         imgLeft.append(UIImage(named: "short-arrow-left.png")!)
         imgLeft.append(UIImage(named: "long-arrow-left.png")!)
+        imgLeft.append(UIImage(named: "chevron-arrow-left.png")!)
         imgRight.append(UIImage(named: "short-arrow-right.png")!)
         imgRight.append(UIImage(named: "long-arrow-right.png")!)
+        imgRight.append(UIImage(named: "chevron-arrow-right.png")!)
         
         imgViewUp.image = imgUp[0]
         imgViewDown.image = imgDown[0]
@@ -41,23 +45,40 @@ class ViewController: UIViewController {
         
         let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
         swipeUp.direction = UISwipeGestureRecognizer.Direction.up
-        swipeUp.numberOfTouchesRequired = numOfTouches
         self.view.addGestureRecognizer(swipeUp)
         
         let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
         swipeDown.direction = UISwipeGestureRecognizer.Direction.down
-        swipeDown.numberOfTouchesRequired = numOfTouches
         self.view.addGestureRecognizer(swipeDown)
         
         let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
         swipeLeft.direction = UISwipeGestureRecognizer.Direction.left
-        swipeLeft.numberOfTouchesRequired = numOfTouches
         self.view.addGestureRecognizer(swipeLeft)
         
         let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGesture(_:)))
         swipeRight.direction = UISwipeGestureRecognizer.Direction.right
-        swipeRight.numberOfTouchesRequired = numOfTouches
         self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeUpMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGestureMulti(_:)))
+        swipeUpMulti.direction = UISwipeGestureRecognizer.Direction.up
+        swipeUpMulti.numberOfTouchesRequired = numOfTouches
+        self.view.addGestureRecognizer(swipeUpMulti)
+        
+        let swipeDownMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGestureMulti(_:)))
+        swipeDownMulti.direction = UISwipeGestureRecognizer.Direction.down
+        swipeDownMulti.numberOfTouchesRequired = numOfTouches
+        self.view.addGestureRecognizer(swipeDownMulti)
+        
+        let swipeLeftMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGestureMulti(_:)))
+        swipeLeftMulti.direction = UISwipeGestureRecognizer.Direction.left
+        swipeLeftMulti.numberOfTouchesRequired = numOfTouches
+        self.view.addGestureRecognizer(swipeLeftMulti)
+        
+        let swipeRightMulti = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.respondToSwipeGestureMulti(_:)))
+        swipeRightMulti.direction = UISwipeGestureRecognizer.Direction.right
+        swipeRightMulti.numberOfTouchesRequired = numOfTouches
+        self.view.addGestureRecognizer(swipeRightMulti)
+        
         
     }
     
@@ -78,6 +99,28 @@ class ViewController: UIViewController {
                 imgViewLeft.image = imgLeft[1]
             case UISwipeGestureRecognizer.Direction.right:
                 imgViewRight.image = imgRight[1]
+            default:
+                break
+            }
+        }
+    }
+    
+    @objc func respondToSwipeGestureMulti(_ gesture: UIGestureRecognizer) {
+        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+            imgViewUp.image = imgUp[0]
+            imgViewDown.image = imgDown[0]
+            imgViewLeft.image = imgLeft[0]
+            imgViewRight.image = imgRight[0]
+            
+            switch swipeGesture.direction {
+            case UISwipeGestureRecognizer.Direction.up:
+                imgViewUp.image = imgUp[2]
+            case UISwipeGestureRecognizer.Direction.down:
+                imgViewDown.image = imgDown[2]
+            case UISwipeGestureRecognizer.Direction.left:
+                imgViewLeft.image = imgLeft[2]
+            case UISwipeGestureRecognizer.Direction.right:
+                imgViewRight.image = imgRight[2]
             default:
                 break
             }
